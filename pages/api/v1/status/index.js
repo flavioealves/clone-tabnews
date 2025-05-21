b34d70a7713daa0062db890300a9176e6a2a70f6
flavioealves <flavioealves@gmail.com>
2025-05-21 00:45:16 +0000

    add jsconfig.json and db status finish

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js"
+import { version } from "react";
 
 async function status(request,response){
-  const result = await database.query("SELECT 1 + 1 AS sum");
-  console.log(result.rows);
-  response.status(200).json({chave:"são acima da média"});
+  const updatedAt = new Date().toISOString();
+  // const client = await database.query("SELECT version() AS version, current_setting('max_connections')::int AS max_connections, (SELECT COUNT(*)::int FROM pg_stat_activity WHERE DATNAME = 'local_db') AS act_connections")
+  const client = await database.query({
+    text: "SELECT version() AS version, current_setting('max_connections')::int AS max_connections, (SELECT COUNT(*)::int FROM pg_stat_activity WHERE DATNAME = $1) AS act_connections",
+    values: [process.env.POSTGRES_DB]
+  })
+  console.log(client)
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: parseInt(client.rows[0].version.split(' ')[1]),
+    max_connections: client.rows[0].max_connections,
+    act_connections: client.rows[0].act_connections
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.version).toEqual(16)
+  
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.max_connections).toEqual(100);
+
+
+  expect(responseBody.act_connections).toBeDefined();
+  expect(responseBody.act_connections).toEqual(1);
+
   expect(response.status).toBe(200);
 })
