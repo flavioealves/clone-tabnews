76c65255d7eb4925edd0d0a79060cd4c5105b74e
flavioealves <flavioealves@gmail.com>
2025-05-28 01:10:04 +0000

    config ssl connection for postgres

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues()
   });
 
   try {    
@@ -25,4 +26,14 @@ async function query(queryObject) {
 
 export default {
   query:query
+};
+
+function getSSLValues() {
+  if(process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
